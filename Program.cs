86a86fade51eb988d3848ffcd5e0894f3b944e01
unknown <souryzetma1144@yahoo.com>
2024-08-12 21:15:41 +0700

    commit 1

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿﻿using System;
+
+namespace PrimeNumberApp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+          Console.WriteLine("\nHien thi cac so nguyen to nho hon 100:");
+
+      bool isPrime = true;
+      for (int i = 2; i <= 100; i++)
+      {
+        for (int j = 2; j <= 100; j++)
+        {
+          if (i != j && i % j == 0)
+          {
+            isPrime = false;
+            break;
+          }
+        }
+
+        if (isPrime)
+        {
+          Console.Write($"{i}  ");
+        }
+        isPrime = true;
+      }
+
+      Console.WriteLine("\n");
+    }
+  }
+}
